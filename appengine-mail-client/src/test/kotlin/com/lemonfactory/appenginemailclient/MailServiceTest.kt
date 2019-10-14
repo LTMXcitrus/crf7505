b614c61e95eae2ea1498b74d724e268e9080e744
matthieu_lemonnier <matthieu.lemonnier@aios.sh>
2019-10-14 18:18:39 +0200

    sending mails by lot

--- appengine-mail-client/src/test/kotlin/com/lemonfactory/appenginemailclient/MailServiceTest.kt
+++ appengine-mail-client/src/test/kotlin/com/lemonfactory/appenginemailclient/MailServiceTest.kt
@@ -3,24 +3,25 @@ package com.lemonfactory.appenginemailclient
 import com.lemonfactory.crf7505.domain.model.CrfMail
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
-import org.junit.jupiter.api.Test
+import org.junit.Test
 import org.mockito.Mockito
+import javax.mail.internet.MimeMessage
 
 class MailServiceTest {
 
     private val sender = Mockito.mock(MailSender::class.java)
     private val mailService= MailServiceImpl(sender)
 
-//    @Test
-//    fun sendMail_oneMail_should_call_sender() {
-//        // Given
-//        val crfMail = CrfMail("sender", "recipient", "subject", "text")
-//
-//        // when
-//        mailService.sendMails(listOf(crfMail))
-//
-//        // Then
-//        verify(sender, times(1)).send(any())
-//    }
+    @Test
+    fun sendMail_oneMail_should_call_sender() {
+        // Given
+        val crfMail = CrfMail("sender", "recipient", "subject", "text")
+
+        // when
+        mailService.sendMails(listOf(crfMail))
+
+        // Then
+        verify(sender, times(1)).send(any<MimeMessage>())
+    }
 
 }
