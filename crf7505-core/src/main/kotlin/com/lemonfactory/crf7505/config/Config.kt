be875b15ff2f2149b843677aba94d5d24469e446
matthieu_lemonnier <matthieu.lemonnier@aios.sh>
2019-10-14 11:27:11 +0200

    mutable configvalues

--- crf7505-core/src/main/kotlin/com/lemonfactory/crf7505/config/Config.kt
+++ crf7505-core/src/main/kotlin/com/lemonfactory/crf7505/config/Config.kt
@@ -3,7 +3,7 @@ package com.lemonfactory.crf7505.config
 import com.lemonfactory.crf7505.repository.ObjectifyDAO
 import org.slf4j.LoggerFactory
 
-class Config(val objectifyDAO: ObjectifyDAO<ConfigValue>) {
+class Config(private val objectifyDAO: ObjectifyDAO<ConfigValue>) {
     private val LOGGER = LoggerFactory.getLogger(Config::class.java)
 
     private val localStorage = mutableMapOf<String, String>()
@@ -17,19 +17,28 @@ class Config(val objectifyDAO: ObjectifyDAO<ConfigValue>) {
     }
 
     fun getEnvRequired(name: String): String {
-        if(localStorage[name].isNullOrEmpty()) {
-            return requiredProperty(name)
+        if(localStorage.containsKey(name)) {
+            return localStorage[name]!!
         }
-        return localStorage[name]!!
+        return requiredProperty(name)
     }
 
     private fun requiredProperty(name: String): String {
-        return try {
-            objectifyDAO.findById<ConfigValue>(name)!!.value
+        try {
+            val configValue = objectifyDAO.findById<ConfigValue>(name)
+            if(configValue == null || configValue.value.isEmpty()) {
+                LOGGER.info("Config value not stored in base")
+                return propertyNotFound(name)
+            }
+            return configValue.value
         } catch (e: KotlinNullPointerException) {
-            LOGGER.error("The property $name is required.")
-            name
+            return propertyNotFound(name)
         }
+    }
+
+    private fun propertyNotFound(name: String): String {
+        LOGGER.error("The property $name is required.")
+        return name
     }
 
 }
